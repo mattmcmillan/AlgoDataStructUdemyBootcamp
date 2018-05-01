d4b9cc11c3e0389443b29b088cb45fd775e05266
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-01 11:33:20 -0700

    Exercise #6: Array chunking Exercise #7: String anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,70 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//solution two
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^A-Z0-9]/ig, "").toLowerCase().split('').sort().join('');
+}
+
+//solution one
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+
+//     if(Object.keys(aCharMap).length!== Object.keys(bCharMap).length){
+//         return false;
+//     }
+
+//     for(let char in aCharMap){
+//         if(aCharMap[char]!==bCharMap[char]){
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {};
+//     for (let char of str.replace(/[^A-Z0-9]/ig, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
+
+//my solution
+// function anagrams(stringA, stringB) {
+//     const stringAClean = stringA.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+//     const stringBClean = stringB.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+
+//     if (stringAClean.length != stringBClean.length) {
+//         return false;
+//     }
+
+//     const stringAMap = {};
+//     const stringBMap = {};
+
+//     for (let i = 0; i < stringAClean.length; i++) {
+//         let stringAChar = stringAClean[i];
+//         let stringBChar = stringBClean[i];
+
+//         stringAMap[stringAChar] = stringAMap[stringAChar]+1 || 1;
+//         stringBMap[stringBChar] = stringBMap[stringBChar]+1 || 1;
+//     }
+
+//     for (let char in stringAMap) {
+//         if (stringAMap[char] != stringBMap[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true
+// }
+
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,57 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//solution two
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, size + index));
+        index += size;
+    }
+    return chunked;
+}
+
+//solution one
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for(let element of array){
+//         const last = chunked[chunked.length - 1];
+
+//         if(!last || last.length === size){
+//             chunked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+
+//     return chunked;
+// }
+
+//my solution
+// function chunk(array, size) {
+//     let outputAray = [];
+//     let chunkOut = [];
+//     let chunkIndex = 0;
+
+//     for (let arrayNum of array) {
+//         if (chunkIndex < size) {
+//             chunkOut.push(arrayNum);
+//             chunkIndex++;
+//             if(chunkIndex == size){
+//                 outputAray.push(chunkOut.slice());
+//                 chunkOut = [];
+//                 chunkIndex = 0;
+//             }
+//         } 
+//     }
+
+//     if(chunkOut.length>0){
+//         outputAray.push(chunkOut.slice());
+//     }
+
+//     return outputAray;
+// }
 
 module.exports = chunk;
