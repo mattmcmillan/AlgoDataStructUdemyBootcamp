d1449f1ad4b49c3a899d27a291dac5b44f2fe447
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-01 09:29:09 -0700

    Excercise #3: reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//Udemy solution one
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+}
+
+//my solution
+// function reverseInt(n) {
+//     let isPositive = Math.sign(n) === 1;
+//     let reversedNumber = parseInt(n.toString().split('').reverse().join(''));
+
+//     if (!isPositive && n !== 0) {
+//         reversedNumber *= -1;
+//     }
+
+//     return reversedNumber;
+// }
+
+reverseInt(-500);
 
 module.exports = reverseInt;
