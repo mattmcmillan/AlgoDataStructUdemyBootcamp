ac0b786f68eb79a0eca2cc504c0624b02d47ff57
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-01 08:50:44 -0700

    Excercise Two Solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,36 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//Udemy solution two
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - 1 -i];
+    });
+}
+
+//Udemy solution one
+// function palindrome(str) {
+//    const reversed = str.split('').reverse().join('');
+//    return str === reversed;
+// }
+
+//my original solution
+// function palindrome(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     for (var i = 0; i < str.length; i++) {
+//         if(reversed[i]!= str[i]){
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+palindrome("papas");
 
 module.exports = palindrome;
