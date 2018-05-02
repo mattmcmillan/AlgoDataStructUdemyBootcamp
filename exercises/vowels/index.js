62f33eb145edd7efcf7a2f30abaf99ba815e0be1
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-02 15:11:02 -0700

    Excercise #11: Count Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//solution two - regex
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+//solution one - iterative
+// function vowels(str) {
+//     let count = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+//my solution
+// function vowels(str) {
+//     const vowels = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1};
+//     const stringArray = str.toLowerCase().split('');
+//     let vowelCounter = 0;
+//     for(let char of stringArray){
+//         if(vowels[char]){
+//             vowelCounter++;
+//         }
+//     }
+
+//     return vowelCounter;
+// }
 
 module.exports = vowels;
