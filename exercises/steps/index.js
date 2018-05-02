5ef07bea3d3b8cdbc0abeeeea603bc03dce19aac
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-02 14:17:15 -0700

    Excercise #9: Console Log Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,56 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//solution two
+function steps(n, row = 0, stair = '') {
+    if(n===row){
+        return;
+    }
+    if(n===stair.length) {
+        console.log(stair);
+        steps(n, row + 1);
+        return;
+    }
+
+    if(stair.length<=row){
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+}
+
+//solution one
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// }
+
+//my solution
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let consoleOut = '';
+//         for(let j = 0; j < n; j++){
+//             if(j<i){
+//                 consoleOut += '#';
+//             } else {
+//                 consoleOut+= ' ';
+//             }
+//         }
+//         console.log(consoleOut);
+//     }
+// }
 
 module.exports = steps;
