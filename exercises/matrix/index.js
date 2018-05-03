6415693d0b988877a9b8c87bd4c17443b7077413
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-03 10:10:06 -0700

    Excercise #12: Spiral Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,129 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// //solution one
+// function matrix(n) {
+//     const results = [];
+//     for (let i = 0; i < n; i++) {
+//         results.push([]);
+//     }
+
+//     let counter = 1;
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+//     let startRow = 0;
+//     let endRow = n - 1;
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         //Top row
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++
+
+//         //Right column
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+
+//         //Bottom Row
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow][i] = counter;
+//             counter++;
+//         }
+//         endRow--;
+
+//         //Left Column
+//         for (let i =  endRow; i >= startRow; i--){
+//             results[i][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     }
+
+//     return results;
+// }
+
+//second time solving after watching solution
+function matrix(n) {
+    const results = [];
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+    }
+
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        //Top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++
+
+        //Right column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        //Bottom Row
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        //Left Column
+        startColumn++;
+    }
+
+    return results;
+}
+
+
+//my incomplete solution
+// function matrix(n) {
+//     const results = [];
+//     for (let i = 0; i < n; i++) {
+//         results.push([]);
+//     }
+
+//     let counter = 1;
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+//     let startRow = 0;
+//     let endRow = n - 1;
+
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endRow--;
+
+//         //for(let i = startColumn)
+//     }
+
+//     return results;
+// }
 
 module.exports = matrix;
