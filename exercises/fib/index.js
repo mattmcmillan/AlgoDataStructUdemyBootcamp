b7bdb1aa3a31fb91dbc2ca99eee965c1a48b9a01
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-03 18:01:10 -0700

    Excercise #13: Calculate Fibonacci number with recursion and memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,58 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn(...args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+//solution two - recursive with memoization
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 2) + fib(n - 1);
+}
+
+fib = memoize(fib);
+
+//solution one - iterative
+// function fib(n) {
+//     const result = [0, 1];
+
+//     for(let i = 2; i<=n; i++){
+//         const a = result[i-1];
+//         const b = result[i-2];
+
+//         result.push(a+b);
+//     }
+
+//     return result[n];
+// }
+
+//my solution
+// function fib(n) {
+//     let resultArray = [0, 1, 1];
+
+//     if (n < 3) {
+//         return resultArray[n];
+//     }
+
+//     for (let i = 3; i <= n; i++) {
+//         resultArray[i] = resultArray[i-1] + resultArray[i-2];
+//     }
+
+//     return resultArray[n];
+// }
 
 module.exports = fib;
