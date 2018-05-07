871b421fc486a6b4c51b4457fca378312fe18b52
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-07 16:01:53 -0700

    Excercise #20 #21: Tree with BF and DF traversals. Calculate tree width.

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let nodeArray = [root];
+    nodeArray.push('s')
+    
+    let counters = [0];
+
+    while (nodeArray.length > 1) {
+        let node = nodeArray.shift();
+        if (node === 's') {
+            counters.push(0);
+            //we have finished the current level, move end of level indicator to end of array
+            nodeArray.push('s');
+
+        } else {
+            nodeArray.push(...node.children);
+            counters[counters.length - 1]++;
+        }
+    }
+
+    return counters;
+}
+
+
 
 module.exports = levelWidth;
