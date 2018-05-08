48447d05aee255a8d0b391160ff2866815df94ba
Matt McMillan <mattmcmillansoftware@gmail.com>
2018-05-08 08:05:27 -0700

    Exercise #23: BST Validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,44 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+//solution one
+function validate(node, min = null, max = null) {
+    if(max!==null && node.data > max){
+        return false;
+    }
+    if(min!==null && node.data < min) {
+        return false;
+    }
+    if(node.left && !validate(node.left, min, node.data)){
+        return false;
+    }
+    if(node.right && !validate(node.right, node.data, max)){
+        return false;
+    }
+
+    return true;
+}
+
+//my solution
+// function validate(node, min = null, max = null) {
+//     if(node.left){
+//         max = node.data;
+//         if(node.left.data < max){
+//             return validate(node.left, min, max);
+//         } else {
+//             return false;
+//         }
+//     }
+//     if(node.right){
+//         min = node.data;
+//         if(node.right.data > min){
+//             return validate(node.right, min, max);
+//         } else {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
 
 module.exports = validate;
